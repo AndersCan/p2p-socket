9c1d3814bb1ea41221bc2f45e0da2692dd64eb9e
Anders Olav Candasamy <anders.can1@gmail.com>
2022-09-02 09:50:55 +0200

    fix esbuild bundling

--- build.ts
+++ build.ts
@@ -1,12 +1,25 @@
 import { build } from "esbuild";
 
+let makeAllPackagesExternalPlugin = {
+  name: "make-all-packages-external",
+  setup(build) {
+    let filter = /^[^.\/]|^\.[^.\/]|^\.\.[^\/]/; // Must not start with "/" or "./" or "../"
+    build.onResolve({ filter }, (args) => ({
+      path: args.path,
+      external: true,
+    }));
+  },
+};
+
 build({
   bundle: true,
+  preserveSymlinks: true,
   entryPoints: ["./bin.ts"],
+  external: ["./node_modules/*"],
   platform: "node",
   format: "esm",
   target: "es2020",
-  external: ["./node_modules/*"],
   outdir: "./build/",
   banner: { js: "#!/usr/bin/env node" },
+  plugins: [makeAllPackagesExternalPlugin],
 });

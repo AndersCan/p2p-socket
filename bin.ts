7f564153a7ce353283a103c8f55783678660ebae
Anders Olav Candasamy <anders.can1@gmail.com>
2022-09-02 10:55:17 +0200

    feat: improve console logs
     - kleur :)

--- bin.ts
+++ bin.ts
@@ -1,10 +1,11 @@
 // TypeScript (.ts)
 import { Command } from "commander";
-import { createWriteStream } from "fs";
+import { createWriteStream, existsSync } from "fs";
 import { readFile } from "fs/promises";
 import { createKeyPair } from "./src/create-key-pair";
 import { createP2PtoTCPProxy } from "./src/create-p2p-to-tcp-proxy.js";
 import { createTCPtoP2PProxy } from "./src/create-tcp-to-p2p-proxy";
+import kleur from "kleur";
 import type { Identity, SerializedIdentity } from "./src/identity";
 
 const program = new Command();
@@ -32,12 +33,25 @@ function addInitCommand(program) {
       []
     )
     .action((options) => {
+      const path = "./identity.json";
+
+      if (hasIdentity()) {
+        console.error(
+          kleur.red(
+            `${path} already exists. Please delete it if you want a new identity`
+          )
+        );
+        return;
+      }
+
       const parsedSeed = options.seed ? options.seed.join("") : undefined;
 
-      const writeStream = createWriteStream("./identity.json");
+      const writeStream = createWriteStream(path);
 
       writeStream.once("close", () => {
-        console.log("done");
+        console.log(
+          kleur.green().bold().underline(`New identity created at ${path}`)
+        );
       });
       const newKeyPair = createKeyPair(parsedSeed);
       const identity: SerializedIdentity = {
@@ -57,19 +71,24 @@ function addConnectCommand(program: Command) {
     .command("connect")
     .description("Connect over P2P network to a shared resource")
     .requiredOption("-k, --remote-key <key>", "[required] Remote Public key")
-    .option("-h, --host <host>", "[optional] Local")
-    .option("-p, --port <port>", "[optional] Local")
+    .option("-h, --host <host>", "[optional] default: localhost", "localhost")
+    .option("-p, --port <port>", "[optional] default: 0", "0")
     .action(async (options) => {
       const { host, port, remoteKey } = options;
 
       await createTCPtoP2PProxy({
         tcp: {
           host,
-          port,
+          port: Number(port),
         },
         remotePublicKey: Buffer.from(remoteKey, "hex"),
       });
-      console.log(`P2P proxy reachable via ${host}:${port}`);
+      console.log(
+        kleur
+          .green()
+          .bold()
+          .underline(`P2P proxy reachable via ${host}:${port}`)
+      );
     });
 }
 
@@ -79,25 +98,35 @@ function addShareCommand(program: Command) {
     .description(
       "Share something with the P2P network. Peers will use your publicKey to connect"
     )
-    .requiredOption("-h, --host <host>", "[required] Host")
     .requiredOption("-p, --port <port>", "[required] Port")
+    .option(
+      "-h, --host <host>",
+      "[optional] host default: localhost",
+      "localhost"
+    )
     .action(async (options) => {
       const identity = await getLocalIdentity();
       const { host, port } = options;
       await createP2PtoTCPProxy({
         tcp: {
           host,
-          port,
+          port: Number(port),
         },
         keyPair: identity.keyPair,
       });
 
-      console.log(
-        `P2P server listening on ${identity.keyPair.publicKey.toString("hex")}`
-      );
+      const remoteKey = identity.keyPair.publicKey.toString("hex");
 
       console.log(
-        `npx p2p-socket connect ${identity.keyPair.publicKey.toString("hex")}`
+        kleur.dim().underline(`You are now sharing ${host}:${port} with P2P`)
+      );
+      console.log();
+      console.log(kleur.bold(`Peers can connect to you by running:`));
+
+      console.log(
+        kleur
+          .italic()
+          .underline(`npx p2p-socket connect --remote-key ${remoteKey}`)
       );
     });
 }
@@ -113,4 +142,9 @@ async function getLocalIdentity(): Promise<Identity> {
       secretKey: Buffer.from(datax.keyPair.secretKey, "hex"),
     },
   };
+}
+
+function hasIdentity() {
+  const path = "./identity.json";
+  return existsSync(path);
 }
